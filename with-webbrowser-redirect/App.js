bd58856aba23d111cebd8c1311f54eceffcf50ae
Tomáš Šesták <tomassestak@seznam.cz>
2021-05-27 16:41:28 +0200

    make webbrowser FC

--- with-webbrowser-redirect/App.js
+++ with-webbrowser-redirect/App.js
@@ -1,121 +1,96 @@
-import * as React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 import * as Linking from 'expo-linking';
 import * as WebBrowser from 'expo-web-browser';
-import Constants from 'expo-constants';
+import * as React from 'react';
+import {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    redirectData: null,
-  };
+const App = () => {
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.header}>Redirect Example</Text>
+	const [redirectData, setRedirectData] = useState(null);
 
-        <Button
-          onPress={this._openBrowserAsync}
-          title="Tap here to try it out with openBrowserAsync"
-        />
+	_handleRedirect = (event) => {
+		if (Constants.platform.ios) {
+			WebBrowser.dismissBrowser();
+		} else {
+			_removeLinkingListener();
+		}
 
-        <Button
-          onPress={this._openAuthSessionAsync}
-          title="Tap here to try it out with openAuthSessionAsync"
-        />
+		let data = Linking.parse(event.url);
 
-        {this._maybeRenderRedirectData()}
-      </View>
-    );
-  }
+		setRedirectData({redirectData: data})
+	};
 
-  _handleRedirect = (event) => {
-    if (Constants.platform.ios) {
-      WebBrowser.dismissBrowser();
-    } else {
-      this._removeLinkingListener();
-    }
 
-    let data = Linking.parse(event.url);
+	// openBrowserAsync requires that you subscribe to Linking events and the
+	// resulting Promise only contains information about whether it was canceled
+	// or dismissed
+	_openBrowserAsync = async () => {
+		try {
+			_addLinkingListener();
+			let result = await WebBrowser.openBrowserAsync(
+				// We add `?` at the end of the URL since the test backend that is used
+				// just appends `authToken=<token>` to the URL provided.
+				`https://backend-xxswjknyfi.now.sh/?linkingUri=${Linking.createURL('/?')}`
+			);
 
-    this.setState({ redirectData: data });
-  };
+			// https://github.com/expo/expo/issues/5555
+			if (Constants.platform.ios) {
+				_removeLinkingListener();
+			}
+			setRedirectData({result})
+		} catch (error) {
+			alert(error);
+			console.log(error);
+		}
+	};
 
-  // openAuthSessionAsync doesn't require that you add Linking listeners, it
-  // returns the redirect URL in the resulting Promise
-  _openAuthSessionAsync = async () => {
-    try {
-      let result = await WebBrowser.openAuthSessionAsync(
-        // We add `?` at the end of the URL since the test backend that is used
-        // just appends `authToken=<token>` to the URL provided.
-        `https://backend-xxswjknyfi.now.sh/?linkingUri=${Linking.createURL("/?")}`
-      );
-      let redirectData;
-      if (result.url) {
-        redirectData = Linking.parse(result.url);
-      }
-      this.setState({ result, redirectData });
-    } catch (error) {
-      alert(error);
-      console.log(error);
-    }
-  };
+	_addLinkingListener = () => {
+		Linking.addEventListener('url', _handleRedirect);
+	};
 
-  // openBrowserAsync requires that you subscribe to Linking events and the
-  // resulting Promise only contains information about whether it was canceled
-  // or dismissed
-  _openBrowserAsync = async () => {
-    try {
-      this._addLinkingListener();
-      let result = await WebBrowser.openBrowserAsync(
-        // We add `?` at the end of the URL since the test backend that is used
-        // just appends `authToken=<token>` to the URL provided.
-        `https://backend-xxswjknyfi.now.sh/?linkingUri=${Linking.createURL("/?")}`
-      );
+	_removeLinkingListener = () => {
+		Linking.removeEventListener('url', _handleRedirect);
+	};
 
-      // https://github.com/expo/expo/issues/5555
-      if (Constants.platform.ios) {
-        this._removeLinkingListener();
-      }
+	_maybeRenderRedirectData = () => {
+		if (!redirectData) {
+			return;
+		}
 
-      this.setState({ result });
-    } catch (error) {
-      alert(error);
-      console.log(error);
-    }
-  };
+		return (
+			<Text style={{marginTop: 30}}>
+				{JSON.stringify(redirectData)}
+			</Text>
+		);
+	}
 
-  _addLinkingListener = () => {
-    Linking.addEventListener("url", this._handleRedirect);
-  };
+	return (
+		<View style={styles.container}>
+			<Text style={styles.header}>Redirect Example</Text>
 
-  _removeLinkingListener = () => {
-    Linking.removeEventListener("url", this._handleRedirect);
-  };
+			<Button
+				onPress={_openBrowserAsync}
+				title="Tap here to try it out with openBrowserAsync"
+			/>
 
-  _maybeRenderRedirectData = () => {
-    if (!this.state.redirectData) {
-      return;
-    }
+			{_maybeRenderRedirectData()}
+		</View>
+	);
+};
 
-    return (
-      <Text style={{ marginTop: 30 }}>
-        {JSON.stringify(this.state.redirectData)}
-      </Text>
-    );
-  };
-}
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 40,
-  },
-  header: {
-    fontSize: 25,
-    marginBottom: 25,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingBottom: 40,
+	},
+	header: {
+		fontSize: 25,
+		marginBottom: 25,
+	},
 });
